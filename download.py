f41ddfbd65b697d19531636365c2c0e588d4e850
Sungorilla <mrqianhuzi@gmail.com>
2023-07-10 23:34:40 -0400

    Add interval timing

--- download.py
+++ download.py
@@ -15,8 +15,17 @@ if response_json_obj['data'] is None:
     print("Stream is not live")
 else:
     print(response_json_str)
+    stream_start = datetime.strptime(response_json_obj['data']['created_at'][0:19], "%Y-%m-%dT%H:%M:%S")
+    current_time_utc = datetime.utcnow()
+    stream_duration = current_time_utc - stream_start
+    print("Stream has been live for " + str(stream_duration))
+    max_record_time = 60 * 60 * 3
+    script_run_interval = 60 * 5
+    # This script is triggered once every 5 minutes but we only need to record once every 3 hours
+    if (stream_duration.total_seconds() % max_record_time > script_run_interval ):
+        print("Job already running")
+        exit()
     playback_url = response_json_obj['data']['playback_url']
     datestr = datetime.now().strftime("%Y%m%d_%H%M%S")
     file_name = datestr + ' - ' + response_json_obj['data']['slug'] + ' [' + str(response_json_obj['data']['id']) + '].ts'
-    timeout_sec = 60 * 60 * 3
-    subprocess.call(['streamlink', playback_url, 'best', '-o', file_name], timeout=timeout_sec)
+    subprocess.call(['streamlink', playback_url, 'best', '-o', file_name], timeout=max_record_time + 60)
