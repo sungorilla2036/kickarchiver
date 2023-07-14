8260b4779dfb173a53b7ca44681554d6c8e4474e
Sungorilla <mrqianhuzi@gmail.com>
2023-07-13 20:32:15 -0400

    limit upload time

--- download.py
+++ download.py
@@ -2,7 +2,9 @@ import subprocess
 from curl_cffi import requests
 import json
 from datetime import datetime
+import os
 
+job_start_time = datetime.now()
 channel_info = "https://kick.com/api/v1/channels/infrared"
 live_link_check = "https://kick.com/api/v2/channels/infrared/livestream"
 
@@ -29,3 +31,15 @@ else:
     datestr = datetime.now().strftime("%Y%m%d_%H%M%S")
     file_name = datestr + ' - ' + response_json_obj['data']['slug'] + ' [' + str(response_json_obj['data']['id']) + '].ts'
     subprocess.call(['streamlink', playback_url, 'best', '-o', file_name], timeout=max_record_time + 60)
+
+    current_time = datetime.now()
+    job_duration = current_time - job_start_time
+    max_job_duration = 60 * 60 * 5.5
+    remaining_time = max_job_duration - job_duration.total_seconds()
+
+    accesskey = os.environ['accesskey']
+    secret = os.environ['secret']
+    bucketname = os.environ['bucketname']
+    # Upload to archive.org
+    # curl -g --location --header 'x-archive-queue-derive:0' --header 'x-amz-auto-make-bucket:1' --header "authorization: LOW $accesskey:$secret" --upload-file "$file" http://s3.us.archive.org/"$bucketname"/"$fileNameEncoded"
+    subprocess.call(['curl', '-g', '--location', '--header', 'x-archive-queue-derive:0', '--header', 'x-amz-auto-make-bucket:1', '--header', f"authorization: LOW {accesskey}:{secret}", '--upload-file', file_name, f"http://s3.us.archive.org/{bucketname}/{file_name}"], timeout=remaining_time)
